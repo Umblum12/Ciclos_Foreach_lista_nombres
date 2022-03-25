27a92e6e54d1dd3c9533b6ab3da06848ccbcc550
Umblum12 <68798247+Umblum12@users.noreply.github.com>
2022-03-25 03:14:52 -0600

    Agregar archivos de proyecto.

---
+++ Ciclos_Foreach_lista_nombres/Program.cs
@@ -0,0 +1,13 @@
+﻿// See https://aka.ms/new-console-template for more information
+List<string> nombres = new List<string>();
+nombres.Add("Juan");
+nombres.Add("Lupita");
+nombres.Add("Martha");
+nombres.Add("Rafael");
+nombres.Add("Alberto");
+nombres.Add("Dolores");
+foreach (string nombre in nombres)
+{
+    Console.WriteLine(nombre);
+}
+Console.ReadKey(true);
